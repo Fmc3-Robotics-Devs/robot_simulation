7296c840510516ee383a75e29c6d486d80263bf1
phl <puheliang122@gmail.com>
2026-07-27 22:57:48 -0400

    fix: handle KeyboardInterrupt in demo joint state publisher shutdown

--- Rviz/src/franzi_moveit_config/scripts/demo_joint_state_publisher.py
+++ Rviz/src/franzi_moveit_config/scripts/demo_joint_state_publisher.py
@@ -191,10 +191,13 @@ def main():
     executor.add_node(node)
     try:
         executor.spin()
+    except KeyboardInterrupt:
+        pass
     finally:
         executor.shutdown()
         node.destroy_node()
-        rclpy.shutdown()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 
 if __name__ == "__main__":
